5b17c697159e74f1defdfb7cc56c7c8a5f675464
qiumohanyu <qiumohanyu>
2016-11-07 16:53:40 +0800

    c

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-/**
- * Created by Stone on 2016/11/7.
- */
+var express = require('express')
+    , bodyParser = require('body-parser')
+    , path = require('path')
+    , cookieParser = require('cookie-parser')
+    , index = require('./routes');
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', 'views');
+
+app.use('/', index);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res) {
+    res.render('index', {title: 'Not Found'});
+});
+app.listen(3000, function (err) {
+    console.log('my server is start,at port %d', 3000);
+})
